ca77c1b53623d8961ee75060a97d1b23bc447400
OlirahetaX <oliveriraheta57@gmail.com>
2023-05-05 14:42:42 -0600

    Segundo commit

--- Lab3P3_OliverIraheta.cpp
+++ Lab3P3_OliverIraheta.cpp
@@ -1,20 +1,123 @@
-// Lab3P3_OliverIraheta.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+void DiscoX0R() {
+    int salir = 0;
+    while (salir != 4) {
+        int disco1[8];
+        int disco2[8];
+        int XOR[8];
+        cout << "\n 1. Guardar datos en XOR\n 2. Obtener datos de disco 1\n 3. Obtener datos de disco 2\n 4. Salir\nIngrese la opcion: ";
+        cin >> salir;
+        switch (salir) {
+        case 1:
+            //Ingresa disco 1
+            cout << "\nIngrese datos de DISCO 1\n";
+            for (int i = 0; i < 8; i++)            {
+                cout << "Disco 1 [" << i << "]: ";
+                cin >> disco1[i];
+            }
+            //ingresa disco 2
+            cout << "\nIngrese datos de DISCO 2\n";
+            for (int i = 0; i < 8; i++) {
+                cout << "Disco 2 [" << i << "]: ";
+                cin >> disco2[i];
+            }
+            //calcula e imprime disco XOR
+            cout << "\nDisco XOR: [ ";
+            for (int i = 0; i < 8; i++)            {
+                if (disco1[i] == disco2[i])
+                    XOR[i] = 0;
+                else
+                    XOR[i] = 1;
+                cout << XOR[i] << " ";
+            }
+            cout << "]\n";
+            break;
+        case 2:
+            //IMprime disco XOR
+            cout << "\nDisco XOR: [ ";
+            for (int i = 0; i < 8; i++) {
+                cout << XOR[i] << " ";
+            }
+            cout << "]\n";
+            //IMprime disco 2
+            cout << "\nDisco 2: [ ";
+            for (int i = 0; i < 8; i++) {
+                cout << disco2[i] << " ";
+            }
+            cout << "]\n";
+            //Calcula e imprime disco 1
+            cout << "\nDatos calculados del disco 1: [ ";
+            for (int i = 0; i < 8; i++) {
+                if (disco2[i] == XOR[i])
+                    disco1[i] = 0;
+                else
+                    disco1[i] = 1;
+                cout << disco1[i] << " ";
+            }
+            cout << "]\n";
+            break;
+        case 3:
+            //IMprime disco XOR
+            cout << "\nDisco XOR: [ ";
+            for (int i = 0; i < 8; i++) {
+                cout << XOR[i] << " ";
+            }
+            cout << "]\n";
+            //IMprime disco 1
+            cout << "\nDisco 1: [ ";
+            for (int i = 0; i < 8; i++) {
+                cout << disco1[i] << " ";
+            }
+            cout << "]\n";
+            //Calcula e imprime disco 2
+            cout << "\nDatos calculados del disco 2: [ ";
+            for (int i = 0; i < 8; i++) {
+                if (disco1[i] == XOR[i])
+                    disco2[i] = 0;
+                else
+                    disco2[i] = 1;
+                cout << disco2[i] << " ";
+            }
+            cout << "]\n";
+            break;
+        }
+    }
 }
+void matrizObs() {
+    int filas = 0, col = 0, obs = 0;
+    cout << "\n --Matriz con Obstaculos--";
+    cout << "Ingrese filas: ";
+    cin >> filas;
+    cout << "Ingrese columnas: ";
+    cin >> col;
+    cout << "Ingrese obstaculos: ";
+    cin >> obs;
+    Llenar_Matriz(filas, col);
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+}
+char** Llenar_Matriz(int fil,int col) {
+        char** matriz = new char* [fil];
+        for (int i = 0; i < fil; i++) {
+            matriz[i] = new char[col];
+            for (int j = 0; j < col; j++) {
+                matriz[i][j] = '-';
+            }
+        }
+        return matriz;
+}
+int main() {
+    int salir = 0;
+    while (salir != 2) {
+        cout << "= = = = = = = = = = = = = = = = = = = = = = = = = = = = =  " << endl << "0) Disco XOR     1) Matriz con Obstáculos        2) Salir\n-> ";
+        cin >> salir;
+        if (salir){
+            matrizObs();
+        }
+        else {
+            DiscoX0R();
+        }
+    }
+    return 0;
+}
